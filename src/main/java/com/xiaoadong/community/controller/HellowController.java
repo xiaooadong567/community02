f1f6c288bc033bb11953395619b16de968b9a80d
xiaoadong <13392686565@163.com>
2020-04-10 11:09:17 +0800

    init repo

---
+++ src/main/java/com/xiaoadong/community/controller/HellowController.java
@@ -0,0 +1,16 @@
+package com.xiaoadong.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HellowController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
