0ea1d59a2a197479cb85ba7f5e0f14077a168778
xiaoadong <13392686565@163.com>
2020-04-17 23:17:06 +0800

    公共导航

--- src/main/java/com/xiaoadong/community/controller/IndexController.java
+++ src/main/java/com/xiaoadong/community/controller/IndexController.java
@@ -31,7 +31,7 @@ public class IndexController {
     public String index(HttpServletRequest request,
                         Model model,
                         @RequestParam(name = "page",defaultValue = "1")Integer page,
-                        @RequestParam(name = "size",defaultValue = "2")Integer size) {
+                        @RequestParam(name = "size",defaultValue = "5")Integer size) {
 
         //获取user信息 验证cookies信息
         Cookie[] cookies = request.getCookies();
